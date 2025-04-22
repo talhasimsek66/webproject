f41ce0586b1376cdbb5cf51b6c365c7172809313
Talha Burak Şimşek <talha.simsek@live.acibadem.edu.tr>
2025-04-23 02:30:28 +0300

    finished except forum

--- passport/urls.py
+++ passport/urls.py
@@ -7,9 +7,6 @@ urlpatterns = [
     path('exchange/',views.exchange),
     path('countries/', views.country_list),
     path('passport/',views.passport_list),
-    path('ordinary/', views.ordinarypass ),
-
-    path('special/', views.specialpass),
     path('filter/', views.filter_by_passport, name='filter_by_passport'),
     path('forum/', views.forum),
 
